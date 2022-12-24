13d6eb4c9742defe9bfe25ee9a6efb2a53fd947a
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2022-12-24 22:38:18 +0530

    actions added

--- TankApp/Controller/ViewController.swift
+++ TankApp/Controller/ViewController.swift
@@ -13,6 +13,37 @@ class ViewController: UIViewController {
     @IBOutlet var arView: ARView!
     var tankAnchor: TinyToyTank._TinyToyTank?
     
+    // MARK: - Actions
+    @IBAction func canonShotPressed(_ sender: Any) {
+        print("DEBUG:: canon Pressed")
+    }
+    
+    @IBAction func forwardButtonPressed(_ sender: Any) {
+        print("DEBUG:: forwardTank Pressed")
+
+    }
+    
+    @IBAction func rightButtonTankPressed(_ sender: Any) {
+        print("DEBUG:: rightTank Pressed")
+
+    }
+    
+    @IBAction func rightButtonCanonPressed(_ sender: Any) {
+        print("DEBUG:: rightCanon Pressed")
+
+    }
+    
+    @IBAction func leftButtonCanonPressed(_ sender: Any) {
+        print("DEBUG:: leftCanon Pressed")
+
+    }
+    
+    @IBAction func leftButtonTankPressed(_ sender: Any) {
+        print("DEBUG:: leftTank Pressed")
+
+    }
+    
+    
     // MARK: - Lifecycle Functions
     
     override func viewDidLoad() {
